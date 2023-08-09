f3eb6297bcc376095235e0808a27422d29209ea8
Ismail Yushaw <isyuus@gmail.com>
2023-08-09 18:28:38 +0000

    Edited app and models

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from database import Base
 
 app = FastAPI()
 
@@ -7,9 +10,23 @@ def read_root():
     return {"message": "Welcome to the Donation App API"}
 
 @app.post("/donate/")
-def create_donation(donor: str, amount: float):
-    donation = {"donor": donor, "amount": amount}
-    donations.append(donation)
+def create_donation(donor_name: str, ethereum_address: str, amount: float):
+    db = SessionLocal()
+    
+    # Check if the donor exists or create a new one
+    donor = db.query(Donor).filter(Donor.ethereum_address == ethereum_address).first()
+    if not donor:
+        donor = Donor(name=donor_name, ethereum_address=ethereum_address)
+        db.add(donor)
+        db.commit()
+        db.refresh(donor)
+    
+    # Create a donation
+    donation = Donation(donor_id=donor.id, amount=amount)
+    db.add(donation)
+    db.commit()
+    db.close()
+    
     return {"message": "Donation created successfully"}
 
 @app.get("/donations/")
--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from database import Base
+
 
 class Donation(Base):
     __tablename__ = "donations"
@@ -9,5 +11,19 @@ class Donation(Base):
     id = Column(Integer, primary_key=True, index=True)
     donor = Column(String, index=True)
     amount = Column(Float)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    donor_id = Column(Integer, ForeignKey('donor.id'))
+    ethereum_address = Column(String, unique=True, index=True)
+    don_key = relationship('Donor')
+
+
+class Donor(Base):
+    __tablename__ = "donors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    
+    donations = relationship("Donation", back_populates="donor")
 
 Base.metadata.create_all(bind=engine)
